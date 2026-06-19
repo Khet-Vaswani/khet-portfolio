043cf88166954ca4d55d6e0cb55db03aae1be0fa
Khet Vaswani <146008319+Khet-Vaswani@users.noreply.github.com>
2026-06-19 12:59:10 +0500

    Update next.config.ts for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "khet-portfolio";
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
     remotePatterns: [
